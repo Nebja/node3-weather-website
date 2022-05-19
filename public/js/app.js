d31c96e068a96371c029e7dc895389679a86b4ac
benja <benjaminkotsaridis@outlook.com.gr>
2022-05-19 16:52:32 +0200

    Init commit

---
+++ public/js/app.js
@@ -0,0 +1,20 @@
+console.log('Client Side js File is loaded')
+
+const weatherForm = document.querySelector('form')
+const search = document.querySelector('input')
+const messageOne = document.getElementById('message-1')
+const messageTwo = document.getElementById('message-2')
+
+weatherForm.addEventListener('submit', (e) => {
+    e.preventDefault()
+    const location = search.value
+    messageOne.textContent = 'Loading...'
+    messageTwo.textContent = ''
+    fetch('http://localhost:3000/weather?address='+ location).then((r) =>{
+        r.json().then((data)=>{
+            if (data.error)return messageOne.textContent = data.error
+            messageOne.textContent = data.location
+            messageTwo.textContent = data.forecast
+        })
+    })
+})
